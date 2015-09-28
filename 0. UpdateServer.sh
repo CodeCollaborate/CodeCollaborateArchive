87a5c3c03b2676b5beee9d844fe1f4cb9f921556
Benedict Wong <BWong@ObsessiveOrange.com>
2015-09-28 17:04:13 -0400

    Added echo websocket server from Gorilla

---
+++ 0. UpdateServer.sh
@@ -0,0 +1,3 @@
+git pull
+go build server.go
+./server
--- server.go
+++ server.go
@@ -1,11 +1,60 @@
+
 package main
 
 import (
-	"fmt"
+	"flag"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
 )
 
+var addr = flag.String("addr", "localhost:8080", "http service address")
+
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+
+} // use default options
+
+func echo(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.Error(w, "Not found", 404)
+		return
+	}
+	if r.Method != "GET" {
+		http.Error(w, "Method not allowed", 405)
+		return
+	}
+	c, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Print("upgrade:", err)
+		return
+	}
+	defer c.Close()
+	for {
+		mt, message, err := c.ReadMessage()
+		if err != nil {
+			log.Println("read:", err)
+			break
+		}
+		log.Printf("recv: %s", message)
+		err = c.WriteMessage(mt, message)
+		if err != nil {
+			log.Println("write:", err)
+			break
+		}
+	}
+}
+
 func main() {
+	flag.Parse()
+	log.SetFlags(0)
 
-	fmt.Println("Hello World!")
-
+	http.HandleFunc("/", echo)
+	err := http.ListenAndServe(*addr, nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
