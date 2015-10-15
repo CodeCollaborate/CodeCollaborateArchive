dfe177382b0fd1191bbf72459579a4b58472be63
Benedict Wong <bennydictwong@gmail.com>
2015-10-15 15:42:57 -0400

    Chnaged main class's name in order to deconflict with folder name

--- build.sh
+++ build.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env sh
 git pull
-go build server.go
+go build CodeCollaborate.go
 sudo cp -f etc/init.d/CodeCollaborate /etc/init.d/
 sudo mkdir -p /CodeCollaborate
 
 sudo service CodeCollaborate stop
-sudo cp -f server /CodeCollaborate
-sudo mv -f /CodeCollaborate/server /CodeCollaborate/CodeCollaborate
+sudo cp -f CodeCollaborate /CodeCollaborate
 sudo service CodeCollaborate start
