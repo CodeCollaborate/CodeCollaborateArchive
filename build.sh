ee1f1192436ad6ff90d265f71bed0e8edd5cfe29
Joel Shapiro <joel.shapiro7@gmail.com>
2015-11-05 14:15:11 -0500

    fixed maven issues

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ echo "Building server binary from go project"
 go build CodeCollaborate.go
 
 echo "Building scrunching jar from source"
-cd Scrunching && mvn package && cd ..
+cd Scrunching && mvn clean package && cd ..
 
 echo "Adding init.d entry"
 sudo cp -f etc/init.d/CodeCollaborate /etc/init.d/
@@ -12,5 +12,5 @@ sudo mkdir -p /CodeCollaborate
 echo "Restarting and updating service"
 sudo service CodeCollaborate stop
 sudo cp -f CodeCollaborate /CodeCollaborate
-sudo cp -f Scrunching/target/Scrunching.jar /CodeCollaborate
+sudo cp -f Scrunching/target/Scrunching-jar-with-dependencies.jar /CodeCollaborate/Scrunching.jar
 sudo service CodeCollaborate start
